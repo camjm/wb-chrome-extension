16218298f43445c7c6e8877f26b5d296421f8000
Cam <cam@mackay.net.nz>
2017-01-22 14:37:42 +1300

    workbench page action

--- app/scripts.babel/background.js
+++ app/scripts.babel/background.js
@@ -1,11 +1,20 @@
 'use strict';
 
-chrome.runtime.onInstalled.addListener(details => {
-  console.log('previousVersion', details.previousVersion);
+// When the extension is installed or upgraded...
+chrome.runtime.onInstalled.addListener(function() {
+  // Replace all the rules...
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+    /// With a new rule...
+    chrome.declarativeContent.onPageChanged.addRules([{
+      // That fires when visiting an instance of the Workbench application...
+      conditions: [
+        new chrome.declarativeContent.PageStateMatcher({
+          pageUrl: { pathSuffix: '/Workbench.aspx' },
+          css: [ 'html[ng-app=wbApp]' ]
+        })
+      ],
+      /// And shows the Workbench extension page action
+      actions: [ new chrome.declarativeContent.ShowPageAction() ]
+    }]);
+  });
 });
-
-chrome.tabs.onUpdated.addListener(tabId => {
-  chrome.pageAction.show(tabId);
-});
-
-console.log('\'Allo \'Allo! Event Page for Page Action');
